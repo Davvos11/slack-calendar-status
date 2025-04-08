6f706d5943292908b6aad37301c15f3be49f9f9f
David Vos <vosdavid2@gmail.com>
2025-04-08 18:48:55 +0200

    Make work event title generic

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,28 @@ use crate::ical::get_events;
 use crate::slack::{Slack, UserPrefs, UserProfile};
 use chrono::{Datelike, Local, TimeZone, Utc};
 use std::process::exit;
+use dotenv::dotenv;
 
 mod ical;
 mod slack;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    // Load env variables from .env
+    dotenv().ok();
+    let work_event = dotenv::var("WORK_EVENT").expect("WORK_EVENT not set");
+    
+    // Load events
     let events = get_events().await?;
 
     let mut prefs = UserPrefs::new();
     let now = Local::now();
     let mut next = None;
     let mut current = false;
+    
+    // Filter and process events
     for event in events {
-        if event.name != "Werk" {
+        if event.name != work_event {
             continue;
         }
 
@@ -39,6 +47,7 @@ async fn main() -> anyhow::Result<()> {
         next.map(UserProfile::out_of_office)
     };
 
+    // Send to Slack
     let slack = Slack::new();
     let result = slack.set_notification_schedule(&prefs).await?;
     println!("{result}");
